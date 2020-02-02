76351d93b2992526e8274b054534134dd2e5871d
Cheng XU <git@xuc.me>
2020-02-02 18:42:32 +0800

    use JsValueError

--- src/lib.rs
+++ src/lib.rs
@@ -34,12 +34,15 @@ thread_local! {
 #[non_exhaustive]
 #[derive(thiserror::Error, Clone, Debug)]
 pub enum Error {
+    /// Error on js context creation. See [`quick_js::ContextError`].
     #[error("failed to initialize js environment (detail: {0})")]
     JsInitError(String),
+    /// Error on js execution. See [`quick_js::ExecutionError`].
     #[error("failed to execute js (detail: {0})")]
     JsExecError(String),
-    #[error("js returns invalid result")]
-    InvalidResult,
+    /// Error on js value conversion. See [`quick_js::ValueError`].
+    #[error("failed to convert js value (detail: {0})")]
+    JsValueError(String),
 }
 
 impl From<quick_js::ContextError> for Error {
@@ -51,6 +54,12 @@ impl From<quick_js::ContextError> for Error {
 impl From<quick_js::ExecutionError> for Error {
     fn from(e: quick_js::ExecutionError) -> Self {
         Self::JsExecError(format!("{}", e))
+    }
+}
+
+impl From<quick_js::ValueError> for Error {
+    fn from(e: quick_js::ValueError) -> Self {
+        Self::JsValueError(format!("{}", e))
     }
 }
 
@@ -206,8 +215,11 @@ pub fn render_with_opts(input: &str, opts: Opts) -> Result<String> {
             Err(e) => return Err(e.clone()),
         };
         let args: Vec<JsValue> = vec![input.into(), opts.into()];
-        let result = ctx.call_function("renderToString", args)?;
-        result.into_string().ok_or_else(|| Error::InvalidResult)
+        let result = ctx
+            .call_function("renderToString", args)?
+            .into_string()
+            .ok_or_else(|| quick_js::ValueError::UnexpectedType)?;
+        Ok(result)
     })
 }
 
