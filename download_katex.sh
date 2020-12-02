dc4b81ede3d7450900341858b9eef5742742bbb3
Cheng XU <git@xuc.me>
2020-12-01 17:06:02 -0800

    chore: update download_katex.sh
    Make download_katex.sh retrieve the latest LICENSE from KaTeX.

--- download_katex.sh
+++ download_katex.sh
@@ -6,6 +6,7 @@ URL="https://github.com/KaTeX/KaTeX/releases/download/${VERSION}/katex.tar.gz"
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 VENDOR_DIR="$ROOT_DIR/vendor"
 
+rm -rf "$VENDOR_DIR"
 mkdir -p "$VENDOR_DIR"
-rm -f "$VENDOR_DIR/katex.min.js"
+curl -L https://raw.githubusercontent.com/KaTeX/KaTeX/master/LICENSE -o "$VENDOR_DIR/KATEX-LICENSE"
 curl -L "$URL" | tar -x -z -C "$VENDOR_DIR" --strip-components 1 -f - katex/katex.min.js
