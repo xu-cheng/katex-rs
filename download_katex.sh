b3b3936d067c5b9eb6e1d063b7f89733847b27f5
Cheng XU <git@xuc.me>
2021-04-29 20:18:07 -0700

    feat: update katex to 0.13.3

--- download_katex.sh
+++ download_katex.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION="v0.12.0"
+VERSION="v0.13.3"
 URL="https://github.com/KaTeX/KaTeX/releases/download/${VERSION}/katex.tar.gz"
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
