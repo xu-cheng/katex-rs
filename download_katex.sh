41eb1493db227d4df61e5a024b6125dff2a7775f
Cheng XU <git@xuc.me>
2022-01-13 12:39:19 -0800

    feat: katex 0.15.2

--- download_katex.sh
+++ download_katex.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION="v0.15.1"
+VERSION="v0.15.2"
 URL="https://github.com/KaTeX/KaTeX/releases/download/${VERSION}/katex.tar.gz"
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
