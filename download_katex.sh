ee10025051e383a9bb2d98de9324cb4726b93ac5
Cheng XU <git@xuc.me>
2023-02-05 18:12:34 +0800

    feat: move KATEX-VERSION to seperated file, upgrade to 0.16.4

--- download_katex.sh
+++ download_katex.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-VERSION="v0.16.3"
-URL="https://github.com/KaTeX/KaTeX/releases/download/${VERSION}/katex.tar.gz"
-
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 VENDOR_DIR="$ROOT_DIR/vendor"
 
+VERSION="$(cat ./KATEX-VERSION)"
+URL="https://github.com/KaTeX/KaTeX/releases/download/v${VERSION}/katex.tar.gz"
+
 rm -rf "$VENDOR_DIR"
 mkdir -p "$VENDOR_DIR"
+echo "download ${URL}..."
 curl -L https://raw.githubusercontent.com/KaTeX/KaTeX/master/LICENSE -o "$VENDOR_DIR/KATEX-LICENSE"
 curl -L "$URL" | tar -x -z -C "$VENDOR_DIR" --strip-components 1 -f - katex/katex.min.js katex/contrib/mhchem.min.js
 
